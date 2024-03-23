eb3697d53cd27f1bd6aef9daf04d203c362c29b7
Gil Tayar <gil@tayar.org>
2024-03-23 12:58:16 +0200

    02 full lesson

--- 02-basic-types/index.ts
+++ 02-basic-types/index.ts
@@ -7,13 +7,19 @@ const o: object = {x: 4};
 const a: any = 1;
 
 n - 4             // OK
+//@ts-expect-error
 n - true          // Error
 
 s.split('')       // OK
+//@ts-expect-error
+s - 5
 
+//@ts-expect-error
 o.x               // Error!
 
 b && !b            // OK
+//@ts-expect-error
+b - 5
 
 a.split('')       // OK
 a + 1             // OK
@@ -41,5 +47,15 @@ class Person {
 
 /// ##### Type Inference
 
-????
+const x = 0; // No need to type
 
+x + 1; // OK
+
+const y = x * 3; // No need to type
+
+// No need to define return value
+function add2(a: number, b: number) {
+  return a + b;
+}
+
+const z = add2(1, 2); // No need to type
